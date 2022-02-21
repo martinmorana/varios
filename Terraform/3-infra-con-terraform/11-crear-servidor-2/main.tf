87b2782ff01f365ab7c94220357c9836a40a0b07
Martín Morana <martinmorana@hotmail.com>
2022-02-21 18:11:01 -0300

    delete key aws

--- Terraform/3-infra-con-terraform/11-crear-servidor-2/main.tf
+++ Terraform/3-infra-con-terraform/11-crear-servidor-2/main.tf
@@ -3,6 +3,6 @@
 # -------------------------
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIARKC7CMC6YYTNMANG"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
