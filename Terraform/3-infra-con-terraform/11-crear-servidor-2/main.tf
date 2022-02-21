ca33d235618ffe60db4f175a47520318b0ae2b6d
Martín Morana <martinmorana@hotmail.com>
2022-02-21 18:08:29 -0300

    Update main.tf

--- Terraform/3-infra-con-terraform/11-crear-servidor-2/main.tf
+++ Terraform/3-infra-con-terraform/11-crear-servidor-2/main.tf
@@ -2,5 +2,7 @@
 # Define el provider de AWS
 # -------------------------
 provider "aws" {
-  region = "eu-west-1"
+  region     = "us-east-1"
+  access_key = "AKIARKC7CMC6YYTNMANG"
+  secret_key = "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"
 }
