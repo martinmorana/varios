6712f3d06cf78ae950170d9f3537859e1fc0027d
Martín Morana <38522374+martinmorana@users.noreply.github.com>
2022-02-23 10:37:57 -0300

    Update main.tf

--- Terraform/Ejemplos/AWS/EC2_ELB/env/prod/main.tf
+++ Terraform/Ejemplos/AWS/EC2_ELB/env/prod/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 }
 
 locals {
-  region          = "eu-west-1"
+  region          = "us-east-1"
   ami             = var.ubuntu_ami[local.region]
   puerto_lb       = 80
   puerto_servidor = 8080
