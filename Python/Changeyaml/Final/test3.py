e9964316ba2bac74b1a2c4b5ac68049d9bd3ccdb
Martin Morana <mmorana@prismamp.com>
2022-10-03 21:11:24 -0300

    update

--- Python/Changeyaml/Final/test3.py
+++ Python/Changeyaml/Final/test3.py
@@ -16,17 +16,19 @@ def read_values():
   
 
 def write_values():
+ 
    with open('output.yaml') as stream2:
       configmap = yaml.safe_load(stream2)
-      #configmap = configmap['configmap']['data']
-      print(configmap)
-      configmap.update(test)
-      print(configmap)
-   with open('output.yaml', "w") as stream2:   
-      yaml.safe_dump(configmap, stream2, default_flow_style=False, explicit_start=False, allow_unicode=True, encoding='utf-8')
-      #yaml.dump(configmap, stream2)
+      configmap2 = configmap['configmap']['data']
+      #configmap.update(test)
+      print(configmap2)
+      configmap2 = ['configmap']['data']
+   #with open('output.yaml', "w") as stream2:   
+      #yaml.safe_dump(configmap, stream2, default_flow_style=False, explicit_start=False, allow_unicode=True, encoding='utf-8')
+      #yaml.dump(test, stream2)
 
 read_values()
+write_values()
 #test.update(dict(variable1=11, variable2=22))
 
   
