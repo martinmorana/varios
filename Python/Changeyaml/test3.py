47d5dcc8d101c6cefdac38ed812e654f2afcb1ff
Martin Morana <mmorana@prismamp.com>
2022-10-06 23:06:38 -0300

    update

--- Python/Changeyaml/test3.py
+++ Python/Changeyaml/test3.py
@@ -1,16 +1,30 @@
 from ctypes.wintypes import PINT
 import yaml
 import os
-# /usr/bin/python3 test3.py
+
 
 AMBIENTE = os.getenv('AMBIENTE')
-print('el entorno es ' + AMBIENTE)
+if AMBIENTE is None:   
+   print('No se declaro la variable de Entorno. Termino script')
+   quit()
+
 MICROSERVICIO = os.getenv('MICROSERVICIO')
-print('el ms es ' + MICROSERVICIO)
+if MICROSERVICIO is None:   
+   print('No se declaro la variable de Microservicio. Termino script')
+   quit()
+
+
+PIPELINE_ENABLED = os.getenv('PIPELINE_ENABLED')
+if PIPELINE_ENABLED is None:   
+   print('Este Microservicio no esta habilitado para configurar el configMap')
+   quit()
+
 MICROSERVICE_YAML = MICROSERVICIO + "/values-main.yaml" 
 MICROSERVICE_CONFIGMAP_YAML = MICROSERVICIO + "/templates/configmap.yaml" 
 
-def read_values():
+print('Vamos a configurar el Microservicio ' + str(MICROSERVICIO) + ' en el ambiente de ' + str(AMBIENTE))
+
+def Change_YAML():
    with open('config.yaml') as yaml_config:
       configmap_input = yaml.safe_load(yaml_config)
    test = configmap_input[AMBIENTE]['configmap']['data']
@@ -24,24 +38,25 @@ def read_values():
       configmap_template_data = yaml.safe_load(configmap_template)
    except yaml.YAMLError as exc:
       print(exc)
-   print(configmap_template_data)
-  
+   
 
    for configmap in test:
-         print(configmap)
-         print(test[configmap])
+         print('Configuro la Variable de Entorno: ' + str(configmap) + ' = '+ str(test[configmap]))
          data['configmap']['data'][configmap.lower()] = test[configmap]
          configmap_template_data['data'][configmap.upper()] =  "{{ default .Values.configmap.data." + str(configmap.lower()) + "}}"
 
 
    with open(MICROSERVICE_YAML, 'w') as yaml_file:
-      yaml_file.write( yaml.dump(data, default_flow_style=False))
+      try:
+         yaml_file.write( yaml.dump(data, default_flow_style=False))
+      except yaml.YAMLError as exc:
+         print(exc)
    
    with open(MICROSERVICE_CONFIGMAP_YAML, 'w') as yaml_file:
-      yaml_file.write( yaml.dump(configmap_template_data, default_flow_style=False))
+      try:
+         yaml_file.write( yaml.dump(configmap_template_data, default_flow_style=False))
+      except yaml.YAMLError as exc:
+         print(exc)
 
-
-
-
-read_values()
+Change_YAML()
 
