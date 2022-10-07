0b298d2969c819e66f8210d4b21062c9196f948a
Martin Morana <mmorana@prismamp.com>
2022-10-06 23:53:17 -0300

    update

--- Python/Changeyaml/test3.py
+++ Python/Changeyaml/test3.py
@@ -3,6 +3,21 @@ import yaml
 import os
 import os.path
 
+
+import sys
+ 
+# total arguments
+n = len(sys.argv)
+print("Total arguments passed:", n)
+ 
+# Arguments passed
+print("\nName of Python script:", sys.argv[0])
+ 
+print("\nArguments passed:", end = " ")
+for i in range(1, n):
+    print(sys.argv[i], end = " ")
+
+
 file_exists = os.path.exists('config.yaml')
 if not file_exists:
    print('NO existe el archivo')
@@ -11,24 +26,24 @@ if not file_exists:
 
 AMBIENTE = os.getenv('AMBIENTE')
 if AMBIENTE is None:   
-   print('No se declaro la variable de Entorno. Termino script')
+   print('\r\nNo se declaro la variable de Entorno. Termino script')
    quit()
 
 MICROSERVICIO = os.getenv('MICROSERVICIO')
 if MICROSERVICIO is None:   
-   print('No se declaro la variable de Microservicio. Termino script')
+   print('\r\nNo se declaro la variable de Microservicio. Termino script')
    quit()
 
 
 PIPELINE_ENABLED = os.getenv('PIPELINE_ENABLED')
 if PIPELINE_ENABLED is None:   
-   print('Este Microservicio no esta habilitado para configurar el configMap')
+   print('\r\nEste Microservicio no esta habilitado para configurar el configMap')
    quit()
 
-MICROSERVICE_YAML = MICROSERVICIO + "/values-main.yaml" 
-MICROSERVICE_CONFIGMAP_YAML = MICROSERVICIO + "/templates/configmap.yaml" 
+microservice_yaml = MICROSERVICIO + "/values-main.yaml" 
+microservice_configmap_yaml = MICROSERVICIO + "/templates/configmap.yaml" 
 
-print('Vamos a configurar el Microservicio ' + str(MICROSERVICIO) + ' en el ambiente de ' + str(AMBIENTE))
+print('\r\nVamos a configurar el Microservicio ' + str(MICROSERVICIO) + ' en el ambiente de ' + str(AMBIENTE))
 
 def Change_YAML():
    with open('config.yaml') as yaml_config:
@@ -38,13 +53,13 @@ def Change_YAML():
          print('Error al abrir el archivo config.yaml ' + str(exc))
    new_values = configmap_input[AMBIENTE]['configmap']['data']   
 
-   values_main_yaml = open(MICROSERVICE_YAML, 'r')
+   values_main_yaml = open(microservice_yaml, 'r')
    try:
       values_main_yaml_data = yaml.safe_load(values_main_yaml)
    except yaml.YAMLError as exc:
       print(exc)
 
-   configmap_template = open(MICROSERVICE_CONFIGMAP_YAML, 'r')
+   configmap_template = open(microservice_configmap_yaml, 'r')
    try:
       configmap_template_data = yaml.safe_load(configmap_template)
    except yaml.YAMLError as exc:
@@ -55,13 +70,13 @@ def Change_YAML():
          values_main_yaml_data['configmap']['data'][configmap.lower()] = new_values[configmap]
          configmap_template_data['data'][configmap.upper()] =  "{{ default .Values.configmap.data." + str(configmap.lower()) + "}}"
 
-   with open(MICROSERVICE_YAML, 'w') as yaml_file:
+   with open(microservice_yaml, 'w') as yaml_file:
       try:
          yaml_file.write( yaml.dump(values_main_yaml_data, default_flow_style=False))
       except yaml.YAMLError as exc:
          print(exc)
    
-   with open(MICROSERVICE_CONFIGMAP_YAML, 'w') as yaml_file:
+   with open(microservice_configmap_yaml, 'w') as yaml_file:
       try:
          yaml_file.write( yaml.dump(configmap_template_data, default_flow_style=False))
       except yaml.YAMLError as exc:
