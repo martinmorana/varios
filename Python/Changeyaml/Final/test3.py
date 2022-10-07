cddae180dcb0336ec21cdb1946e3c09d3d4ec14e
Martin Morana <mmorana@prismamp.com>
2022-10-06 21:28:42 -0300

    delete comments

--- Python/Changeyaml/Final/test3.py
+++ Python/Changeyaml/Final/test3.py
@@ -32,26 +32,4 @@ def read_values():
  
 
 
-  
-   
-
-
-      
 read_values()
-
-#write_values()
-#test.update(dict(variable1=11, variable2=22))
-
-
-""" with open('values-main.yaml') as f:
-    doc = yaml.load(f)
-    # values = doc['components']['star']['init'][0]['values2']
-    # values['logg'] = "pwee"
-    # with open(f'config-2.yaml', 'w') as out:
-    #     yaml.dump(doc, out)
-    values = doc['configmap']['data']
-    values['variable1'] = "pwee"
-    values['variable2'] = "pwee"
-    with open(f'values-main.yaml', 'w') as out:
-        yaml.dump(doc, out) """
-  
